8ba43c77767c45cd85f60a70d21a6b8544988b1e
Aditya Saxena <maverick@localhost.localdomain>
2023-02-22 17:48:50 +0530

    complete

--- di/main.js
+++ di/main.js
@@ -1,8 +1,11 @@
 const awilix = require('awilix');
+const Crypt = require('../utility/crypt.js');
 
 const container = awilix.createContainer();
 container.register({
-    allowedUpdates: awilix.asValue(['firstName', 'lastName', 'age'])
+    allowedUpdates: awilix.asValue(['firstName', 'lastName', 'age']),
+    encryptedFields: awilix.asValue(new Set(['firstName', 'lastName', 'phoneNumber', 'email'])),
+    crypt: awilix.asValue(new Crypt())
 });
 
 module.exports = container;
